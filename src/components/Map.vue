6598a5dfb69b07d1ffca0b47298a37431a1f1120
MG <michael-guenther.goetz@stud.uni-bamberg.de>
2019-06-29 17:22:47 +0200

    Add vue leaflet

---
+++ src/components/Map.vue
@@ -0,0 +1,52 @@
+<template>
+    <div>
+        <div class="info" style="height: 15%">
+            <span>Center: {{ center }}</span>
+            <span>Zoom: {{ zoom }}</span>
+            <span>Bounds: {{ bounds }}</span>
+        </div>
+        <l-map
+                style="height: 400px; width: 400px"
+                :zoom="zoom"
+                :center="center"
+                @update:zoom="zoomUpdated"
+                @update:center="centerUpdated"
+                @update:bounds="boundsUpdated"
+        >
+            <l-tile-layer :url="url"></l-tile-layer>
+            <l-marker :lat-lng="center"></l-marker>
+        </l-map>
+    </div>
+</template>
+
+<script>
+    import {LMap, LTileLayer, LMarker} from 'vue2-leaflet'
+
+    export default {
+        name: "Map",
+        components: { LMap, LTileLayer, LMarker },
+        data() {
+            return {
+                url: 'http://{s}.tile.osm.org/{z}/{x}/{y}.png',
+                zoom: 10,
+                center: [49.8900562,  10.913047],
+                bounds: null
+            }
+        },
+        methods: {
+            zoomUpdated(zoom) {
+                this.zoom = zoom;
+            },
+            centerUpdated(center) {
+                this.center = center;
+            },
+            boundsUpdated(bounds) {
+                this.bounds = bounds;
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
--- src/main.js
+++ src/main.js
@@ -2,11 +2,23 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import 'leaflet/dist/leaflet.css'
+import 'leaflet/dist/leaflet.css'
 
-Vue.config.productionTip = false
+// fix icon for marker
+import { Icon } from 'leaflet'
+delete Icon.Default.prototype._getIconUrl;
+
+Icon.Default.mergeOptions({
+  iconRetinaUrl: require('leaflet/dist/images/marker-icon-2x.png'),
+  iconUrl: require('leaflet/dist/images/marker-icon.png'),
+  shadowUrl: require('leaflet/dist/images/marker-shadow.png')
+});
+
+Vue.config.productionTip = false;
 
 new Vue({
   router,
   store,
   render: h => h(App)
-}).$mount('#app')
+}).$mount('#app');
