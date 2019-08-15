01558d90afbcac4a687bcf3595d111f0275562a5
Michael Götz <michael-guenther.goetz@stud.uni-bamberg.de>
2019-08-16 00:22:15 +0200

    Test geolocation watcher

--- src/components/Map.vue
+++ src/components/Map.vue
@@ -46,26 +46,59 @@
             boundsUpdated(bounds) {
                 this.bounds = bounds;
             },
+            geo_success(position) {
+                console.log("Location Changed");
+                this.$store
+                    .dispatch('setUserPosition', {'user_position': [position.coords.latitude, position.coords.longitude]})
+                    .then();
+            },
+            geo_error() {
+                alert("Entschuldigung, keine Positionsinformationen sind verfügbar.");
+            },
         },
         created() {
-            const locationOptions = {
-                enableHighAccuracy: true, //defaults to false
-                timeout: 5000, //defaults to Infinity
-                maximumAge: 0 //defaults to 0
-
-            };
-            this.$getLocation(locationOptions)
-                .then(coordinates => {
-                    this.$store
-                        .dispatch('setUserPosition', {'user_position': [coordinates.lat, coordinates.lng]})
-                        .then();
-                });
-            this.$watchLocation(locationOptions)
-                .then(coordinates => {
-                    this.$store
-                        .dispatch('setUserPosition', {'user_position': [coordinates.lat, coordinates.lng]})
-                        .then();
-                });
+            // if ("geolocation" in navigator) {
+            //     /* geolocation funktioniert */
+            //
+            //
+            //     var geo_options = {
+            //         enableHighAccuracy: true,
+            //         maximumAge: 30000,
+            //         timeout: 27000
+            //     };
+            //     var watchID = navigator.geolocation.watchPosition(this.geo_success, this.geo_error, geo_options);
+            //     console.log(watchID)
+            // } else {
+            //     /* geolocation funktioniert NICHT */
+            //     console.error("Geolocation not provided!")
+            // }
+            // const locationOptions = {
+            //     enableHighAccuracy: true, //defaults to false
+            //     timeout: 5000, //defaults to Infinity
+            //     maximumAge: 0 //defaults to 0
+            //
+            // };
+            // this.$getLocation(locationOptions)
+            //     .then(coordinates => {
+            //         this.$store
+            //             .dispatch('setUserPosition', {'user_position': [coordinates.lat, coordinates.lng]})
+            //             .then();
+            //     });
+            navigator.geolocation.getCurrentPosition(this.geo_success, this.geo_error, {
+                enableHighAccuracy: true,
+                maximumAge: 4000, // should be default, just in case
+                timeout: 5000
+            });
+            // this.$watchLocation(locationOptions)
+            //     .then(coordinates => {
+            //         console.log(coordinates);
+            //     })
+            // this.$watchLocation(locationOptions)
+            //     .then(coordinates => {
+            //         this.$store
+            //             .dispatch('setUserPosition', {'user_position': [coordinates.lat, coordinates.lng]})
+            //             .then();
+            //     });
         }
     }
 </script>
