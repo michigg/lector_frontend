8969e4113937c9d455b66d61554a101bd7484840
Michael Götz <michael-guenther.goetz@stud.uni-bamberg.de>
2019-08-17 05:07:44 +0200

    Removed unused tab

--- src/views/NavigationBar.vue
+++ src/views/NavigationBar.vue
@@ -5,9 +5,6 @@
                 <router-link to="/">Home</router-link>
             </b-nav-item>
             <b-nav-item>
-                <router-link to="/graph">Graph</router-link>
-            </b-nav-item>
-            <b-nav-item>
                 <router-link to="/open-spaces">Open Spaces</router-link>
             </b-nav-item>
             <b-nav-item>
